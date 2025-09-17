ccca1939349c65a4c081610cabab4d305f60e7f2
LeonardoGael13 <stranshiro13@gmail.com>
2025-09-17 07:38:17 -0600

    Comentarios

---
+++ UNIDAD 2/INTRODUCCION A PYTHON/datosconcom.py
@@ -0,0 +1,13 @@
+#Este programa imprime la variable. Tambien observamos la mutabilidad de python
+
+entero = 10
+flotante = 3.14
+cadena = "Leo Gael"
+booleana = True
+
+print(entero)
+print(flotante)
+print(cadena)
+print(True)
+#Leo Gael
+#ITPA
