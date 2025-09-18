8d0c6aab4c5ee8e19aa4176199c4537152857721
LeonardoGael13 <stranshiro13@gmail.com>
2025-09-18 07:56:50 -0600

    Listas

---
+++ UNIDAD 2/INTRODUCCION A PYTHON/Listas.py
@@ -0,0 +1,15 @@
+#Hola como esta?
+lista=["Juan","Ivan","Adrian","Jair"]
+print (lista)
+#Hello
+print (lista [2])
+#xD
+lista.append ("Alexa")
+print (lista)
+lista.insert( 1, "Javier")
+print (lista)
+#Que hace
+lista.remove ("Juan")
+print (lista)
+lista.pop(1)
+print (lista)
