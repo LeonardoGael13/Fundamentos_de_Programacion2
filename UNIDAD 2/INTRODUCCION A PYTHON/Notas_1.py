5ac69cd2d8e403396ec13a8c4138960529d90d67
LeonardoGael13 <stranshiro13@gmail.com>
2025-09-29 08:03:57 -0600

    Notas1

---
+++ UNIDAD 2/INTRODUCCION A PYTHON/Notas_1.py
@@ -0,0 +1,93 @@
+{
+  "metadata": {
+    "kernelspec": {
+      "name": "xpython",
+      "display_name": "Python 3.13 (XPython)",
+      "language": "python"
+    },
+    "language_info": {
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "version": "3.13.1"
+    }
+  },
+  "nbformat_minor": 5,
+  "nbformat": 4,
+  "cells": [
+    {
+      "id": "227a3728-8bc7-4342-a585-1227a2343737",
+      "cell_type": "code",
+      "source": "print(\"Hola mundo\")",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [
+        {
+          "name": "stdout",
+          "output_type": "stream",
+          "text": "Hola mundo\n"
+        }
+      ],
+      "execution_count": 1
+    },
+    {
+      "id": "df10da30-8c78-41c7-b9d9-583686610da5",
+      "cell_type": "code",
+      "source": "numero=10\nif numero>=5:\n    print(f\"El numero es mayor a 5\")",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [
+        {
+          "name": "stdout",
+          "output_type": "stream",
+          "text": "El numero es mayor a 5\n"
+        }
+      ],
+      "execution_count": 19
+    },
+    {
+      "id": "6c975792-bd9d-4141-845c-76db79016f36",
+      "cell_type": "code",
+      "source": "edad=16\nif edad >18:\n    print(f\"Eres mayor de edad\")\nelif edad== 18:\n    print (f\"Vaya a sacar su INE\")\nelse:\n   print (f\"Eres menor de edad\")",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [
+        {
+          "name": "stdout",
+          "output_type": "stream",
+          "text": "Eres menor de edad\n"
+        }
+      ],
+      "execution_count": 46
+    },
+    {
+      "id": "939d14bb-46a4-4186-b918-6de8da3d7f23",
+      "cell_type": "code",
+      "source": "for i in range(5):\n    print(\"Contador\", i)",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [
+        {
+          "name": "stdout",
+          "output_type": "stream",
+          "text": "Contador 0\nContador 1\nContador 2\nContador 3\nContador 4\n"
+        }
+      ],
+      "execution_count": 48
+    },
+    {
+      "id": "168a7863-cd7a-41af-8f1e-b1d2730c4606",
+      "cell_type": "code",
+      "source": "",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [],
+      "execution_count": null
+    }
+  ]
+}
